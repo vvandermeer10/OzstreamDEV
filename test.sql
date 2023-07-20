b497ab6a105f56b0915e35f75e749742eb7330a8
vvandermeer10 <128372583+vvandermeer10@users.noreply.github.com>
2023-07-21 03:45:27 +1000

    Upload 3
    blah

--- test.sql
+++ test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 20, 2023 at 07:35 PM
+-- Generation Time: Jul 20, 2023 at 07:44 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -20,6 +20,27 @@ SET time_zone = "+00:00";
 --
 -- Database: `test`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aknkadkad`
+--
+
+CREATE TABLE `aknkadkad` (
+  `sssldd` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ggeetsdjs`
+--
+
+CREATE TABLE `ggeetsdjs` (
+  `aoska_fjf` int(11) NOT NULL,
+  `sd_slds_` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
